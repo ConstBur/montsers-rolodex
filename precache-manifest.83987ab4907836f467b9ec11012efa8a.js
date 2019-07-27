de1ff6e28fcd3a30464b98558a79018ef7fd1a06
Constantin Buruiana <constbur@gmail.com>
2019-07-27 13:51:47 -0400

    Updates

---
+++ precache-manifest.83987ab4907836f467b9ec11012efa8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9e2458b5486bb7271ef14d92eaef562c",
+    "url": "/montsers-rolodex/index.html"
+  },
+  {
+    "revision": "b8eddc66ba6d14a65fe6",
+    "url": "/montsers-rolodex/static/css/main.fa44a433.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/montsers-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "b8eddc66ba6d14a65fe6",
+    "url": "/montsers-rolodex/static/js/main.4baa35c1.chunk.js"
+  },
+  {
+    "revision": "89547f1d34aad326ea39",
+    "url": "/montsers-rolodex/static/js/runtime~main.30f3caa1.js"
+  }
+]);
